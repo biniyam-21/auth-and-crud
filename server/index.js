21785a8180b16685904de555a571ee42e62dff33
Biniyam <biniyamxyz@gmail.com>
2024-07-07 03:26:51 +0300

    routing and db connection done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { UserRouter } from "./routes/user.js";
+import cors from 'cors'
 dotenv.config()
 
 
-
 const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use('/auth', UserRouter)
+mongoose.connect("mongodb://localhost:27017/authentication");
+
+
+
+
  app.listen(process.env.PORT, () =>{
 console.log(`server is stated running on port ${process.env.PORT}`)
  })
